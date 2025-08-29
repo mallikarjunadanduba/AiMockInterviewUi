1c0638b4c878998a479ca7ed30816f2dc1353fcc
Mallikarjun S <mallikarjuns5995@gmail.com>
2025-08-29 16:49:28 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Mainroutes from './routes/Mainroutes';
-import '../src/app.css'
+import "./App.css";
 
 function App() {
   return (
